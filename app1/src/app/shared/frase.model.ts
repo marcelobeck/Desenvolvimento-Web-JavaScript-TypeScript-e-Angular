2e6a297b55e2f5b82742fb7408065066c76b3050
marcelobeck <desenvolvimento4@brazilsistem.com.br>
2024-02-07 22:22:05 -0300

    Angular

---
+++ app1/src/app/shared/frase.model.ts
@@ -0,0 +1,11 @@
+export class Frase { // Essa classe é usada para tipo os objetos detro do codigo
+    // public fraseEng:string
+    // public frasePtBr: string
+
+    // constructor(fraseEng: string, frasePtBr:string){
+    //     this.fraseEng = fraseEng
+    //     this.frasePtBr = frasePtBr
+    // }
+    constructor(public fraseEng: string, public frasePtBr: string){
+    }
+}
